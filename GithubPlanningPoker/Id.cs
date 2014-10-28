ddcfe064b21ac37e2cce1538982dbf293bc68cb0
deaddog <mikkelslsl@gmail.com>
2014-10-28 18:19:15 +0100

    Created a class for handling id-hashes.

---
+++ GithubPlanningPoker/Id.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubPlanningPoker
+{
+    public class Id
+    {
+        private readonly string hash;
+
+        public Id(string hash)
+        {
+            this.hash = hash;
+        }
+
+        public string Hash
+        {
+            get { return hash; }
+        }
+    }
+}
